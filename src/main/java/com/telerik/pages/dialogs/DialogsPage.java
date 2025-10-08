69cfcf51b7fd1ef6c1e84a02d7defe346a064b74
OlgaFilili <philippova.ov@gmail.com>
2025-10-08 13:09:14 +0300

    feat: update DialogsPage

--- src/main/java/com/telerik/pages/dialogs/DialogsPage.java
+++ src/main/java/com/telerik/pages/dialogs/DialogsPage.java
@@ -9,13 +9,20 @@ import static utilities.Utils.*;
 
 public class DialogsPage extends BasePage {
     private By opendialogButton= By.xpath("//span[text()='Open dialog']");
-    private By openwindowButton= By.xpath("//span[text()='Open window']");
-    private By opendialogTitle= By.cssSelector("span[id*='kendo-dialog-title-']");
+    private By dialogTitle= By.cssSelector("span[id*='kendo-dialog-title-']");
     //private By closedialogButton=By.xpath("//button[@title='Close']");
-    private By opendialogText= By.cssSelector("div[id*='kendo-dialog-content-'] p");
-    private By opendialogNoButton= By.xpath("//kendo-dialog-actions//span[text()='No']");
-    private By opendialogYesButton= By.xpath("//kendo-dialog-actions//span[text()='Yes']");
-    private By opendialogYesButtonColorTheme= By.cssSelector("button[themecolor='primary']");
+    private By dialogText= By.cssSelector("div[id*='kendo-dialog-content-'] p");
+    private By dialogNoButton= By.xpath("//kendo-dialog-actions//span[text()='No']");
+    private By dialogYesButton= By.xpath("//kendo-dialog-actions//span[text()='Yes']");
+    private By dialogYesButtonColorTheme= By.cssSelector("button[themecolor='primary']");
+
+    private By openwindowButton= By.xpath("//span[text()='Open window']");
+    private By windowTitle= By.cssSelector("span[id*='kendo-window-title-']");
+    private By windowText= By.xpath("//p[text()='Additional info']");
+    private By minimizeWindowButton= By.xpath("//button[@title='Minimize']");
+    private By maximizeWindowButton= By.xpath("//button[@title='Maximize']");
+    private By closeWindowButton= By.xpath("//button[@title='Close']");
+    private By restoreWindowButton= By.xpath("//button[@title='Restore']");
 
     public DialogsPage(WebDriver driver){
         super(driver);
@@ -26,43 +33,68 @@ public class DialogsPage extends BasePage {
         return opendialogButton;
     }
 
+    public boolean mainElementIsDisplayed() {
+        return find(opendialogButton).isDisplayed();
+    }
+
     public void clickOpendialogButton(){
         click(opendialogButton);
     }
-
-    public String getTitle(){
-        return find(opendialogTitle).getText();
+    public String getDialogTitle(){
+        return find(dialogTitle).getText();
     }
-    public String getText(){
-        return find(opendialogText).getText();
+    public String getDialogText(){
+        return find(dialogText).getText();
     }
-
-    public void pressCloseButton(){
+    public boolean dialogNoButtonIsDisplayed(){
+        return find(dialogNoButton).isDisplayed();
+    }
+    public boolean dialogYesButtonIsDisplayed(){
+        return find(dialogYesButton).isDisplayed();
+    }
+    public String colorVerify() {
+        return getColorValue(dialogYesButtonColorTheme, "background-color");
+    }
+    public void closeDialogByKeyboard(){
         Actions act=new Actions(driver);
         act.sendKeys(Keys.chord(Keys.TAB, Keys.ENTER)).perform();
     }
-
-    public boolean opendialogNoButtonIsDisplayed(){
-        return find(opendialogNoButton).isDisplayed();
-    }
-
-    public boolean opendialogYesButtonIsDisplayed(){
-        return find(opendialogYesButton).isDisplayed();
-    }
-
-    public String ColorVerify() {
-        return getColorValue(opendialogYesButtonColorTheme, "background-color");
-    }
-
-    public void returnToMainPage(){
-        waitUntilInvisible(driver, opendialogTitle, 5);
+    public void returnToMainPageFromDialog(){
+        waitUntilInvisible(driver, dialogTitle, 5);
         waitUntilVisible(driver, opendialogButton, 5);
     }
-    public boolean isMainElementVisible() {
-        return find(opendialogButton).isDisplayed();
-    }
+
 
     public void clickOpenwindowButton(){
         click(openwindowButton);
     }
+    public String getWindowTitle(){
+        return find(windowTitle).getText();
+    }
+    public String getWindowText(){
+        return find(windowText).getText();
+    }
+    public boolean windowMininizeButtonIsDisplayed(){
+        return find(minimizeWindowButton).isDisplayed();
+    }
+    public boolean windowMaximizeButtonIsDisplayed(){
+        return find(maximizeWindowButton).isDisplayed();
+    }
+    public boolean windowCloseButtonIsDisplayed(){
+        return find(closeWindowButton).isDisplayed();
+    }
+    public boolean windowRestoreButtonIsDisplayed(){
+        return find(restoreWindowButton).isDisplayed();
+    }
+    public void clickMaximizeButton(){
+        click(maximizeWindowButton);
+    }
+    public void clickCloseButton(){
+        click(closeWindowButton);
+    }
+    public void returnToMainPageFromWindow(){
+        waitUntilInvisible(driver, windowTitle, 5);
+        waitUntilVisible(driver, opendialogButton, 5);
+    }
+
 }
