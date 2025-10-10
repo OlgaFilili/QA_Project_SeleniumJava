c06f9d3d86d7139b72b9b0c9751b47b626cb11dd
OlgaFilili <philippova.ov@gmail.com>
2025-10-10 13:20:22 +0300

    refactor: minor code cleanup in BasePage

--- src/main/java/com/telerik/pages/BasePage.java
+++ src/main/java/com/telerik/pages/BasePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public abstract class BasePage
 {
@@ -15,7 +16,7 @@ public abstract class BasePage
     protected WebDriverWait wait;
     public BasePage(WebDriver driver){
         this.driver=driver;
-        this.wait= new WebDriverWait(driver, Duration.ofSeconds(5));
+        this.wait= new WebDriverWait(driver, Duration.ofSeconds(10));
     }
     // Each page defines its own identifier
     protected abstract By getPageIdentifier();
@@ -24,15 +25,22 @@ public abstract class BasePage
         wait.until(ExpectedConditions.visibilityOfElementLocated(getPageIdentifier()));
     }
 
-
     protected WebElement find(By locator){
         return driver.findElement(locator);
+    }
+    protected List<WebElement> finds(By locator){
+        return driver.findElements(locator);
     }
     protected void click(By locator){
         find(locator).click();
     }
-    public String getColorValue(By locator, String CSSProperty) {
+    protected String getColorValue(By locator, String CSSProperty) {
         String colorCode= find(locator).getCssValue(CSSProperty);
         return Color.fromString(colorCode).asRgb();
     }
+    protected void set(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
 }
