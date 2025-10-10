6514f60bfaa63d098aa5fe11a76fd4d4c56c3853
OlgaFilili <philippova.ov@gmail.com>
2025-10-10 05:32:32 +0300

    refactor: minor code cleanup in BaseTest (no functional changes)

--- src/test/java/task1_2/com/telerik/base/BaseTest.java
+++ src/test/java/task1_2/com/telerik/base/BaseTest.java
@@ -7,8 +7,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-import java.time.Duration;
-
 public class BaseTest {
     protected WebDriver driver;
     protected DialogsPage dialogsPage;
